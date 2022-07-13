927d87465f8e14ad82a115eb42b6f1eaa0ed5520
w2216 <2216459757@qq.com>
2022-07-13 22:25:18 +0800

    格式化代码

--- router/router.go
+++ router/router.go
@@ -1,20 +1,20 @@
 package router
 
 import (
-   "gin_jwt/controller"
-   "gin_jwt/middelware"
-   "github.com/gin-gonic/gin"
+	"gin_jwt/controller"
+	"gin_jwt/middelware"
+	"github.com/gin-gonic/gin"
 )
 
-func Router(r *gin.Engine)  {
+func Router(r *gin.Engine) {
 
-   //用户登录
-   r.GET("/login", controller.LoginController)
+	//用户登录
+	r.GET("/jwt/login", controller.LoginController)
 	// 获取token
-   r.GET("/jwt", controller.JwtController)
-   //使用中间件
-   r.Use(middelware.JWTAuth())
+	r.GET("/jwt/index", controller.JwtController)
+	//使用中间件
+	r.Use(middelware.JWTAuth())
 
-   //获取列表数据
-   r.GET("/list", controller.UserListController)
+	//获取列表数据
+	r.GET("/jwt/list", controller.UserListController)
 }
