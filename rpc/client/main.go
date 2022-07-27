93f3e83dd79a73a561c77934fd8ea9d93aade689
sudongdong <2216459757@qq.com>
2022-07-27 10:58:21 +0800

    rpc模型

---
+++ rpc/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+	"time"
+)
+
+func main() {
+	// 1. 用 rpc 连接服务器 -- Dial
+	conn, err1 := rpc.Dial("tcp", "127.0.0.1:8080")
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+	defer conn.Close()
+
+	// 2. 调用远程函数
+	var reply string
+	timeStr:= time.Now().Format("2006-01-02 15:04:05")
+	err2 := conn.Call("hello.SayHello", "张三 " + timeStr, &reply)
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println(reply)
+}
+
+// 总结
+	// 1. conn, err1 := rpc.Dial("tcp", "127.0.0.1:8080")
+	// 2. err2 := conn.Call("hello.SayHello", "张三 " + timeStr, &reply)
---
+++ rpc/server/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+)
+
+type Hello struct {
+	Name string
+}
+
+// 服务方法
+func (h Hello) SayHello(req string, res *string) error {
+	fmt.Println(req)
+	*res = "你好" + req
+	return nil
+}
+
+// server端
+func main() {
+	// 1. 注册一个服务
+	err1 := rpc.RegisterName("hello", &Hello{})
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+
+	// 2. 设置监听
+	listener, err2 := net.Listen("tcp", "127.0.0.1:8080")
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		fmt.Println("server: 开始工作...")
+		// 3. 接收连接
+		conn, err3 := listener.Accept()
+		if err3 != nil {
+			fmt.Println(err3)
+			return
+		}
+
+		// 4. 绑定业务
+		go rpc.ServeConn(conn)
+
+	}
+}
+
+// 总结
+// 1. err1 := rpc.RegisterName("hello", &Hello{})
+// 2. listener, err2 := net.Listen("tcp", "127.0.0.1:8080")
+// 3. conn, err3 := listener.Accept()
+// 4. go rpc.ServeConn(conn)
