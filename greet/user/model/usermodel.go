46b18ffdcac7ba90bbf1976fd62da9e1e1804231
w2216 <2216459757@qq.com>
2023-03-25 12:11:41 +0800

    Changes mysql

---
+++ greet/user/model/usermodel.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"context"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/sqlc"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ UserModel = (*customUserModel)(nil)
+
+type (
+	// UserModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customUserModel.
+	UserModel interface {
+		userModel
+		FindOneByName(ctx context.Context, name string) (*User, error)
+	}
+
+	customUserModel struct {
+		*defaultUserModel
+	}
+)
+
+// NewUserModel returns a model for the database table.
+func NewUserModel(conn sqlx.SqlConn) UserModel {
+	return &customUserModel{
+		defaultUserModel: newUserModel(conn),
+	}
+}
+
+func (m *defaultUserModel) FindOneByName(ctx context.Context, name string) (*User, error) {
+	query := fmt.Sprintf("select %s from %s where `name` = ? limit 1", userRows, m.table)
+	var resp User
+	err := m.conn.QueryRowCtx(ctx, &resp, query, name)
+	switch err {
+	case nil:
+		return &resp, nil
+	case sqlc.ErrNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
